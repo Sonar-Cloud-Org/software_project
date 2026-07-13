e0744a55f31f7e87b5e5a288044c6db12a0a4bea
Yousef <fellenkigdomee@gmail.com>
2026-07-14 00:16:31 +0300

    exception code

---
+++ vehicle-rental-project/src/main/java/edu/najah/software/exception/AuthenticationException.java
@@ -0,0 +1,10 @@
+package edu.najah.software.exception;
+
+
+public class AuthenticationException extends Exception {
+
+    
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
---
+++ vehicle-rental-project/src/main/java/edu/najah/software/exception/RentalException.java
@@ -0,0 +1,10 @@
+package edu.najah.software.exception;
+
+
+public class RentalException extends Exception {
+
+    
+    public RentalException(String message) {
+        super(message);
+    }
+}
