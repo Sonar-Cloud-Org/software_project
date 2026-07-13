a82e62b66ad95166e8d4ed7a2aa897eb9c657720
Yousef <fellenkigdomee@gmail.com>
2026-07-14 00:26:37 +0300

    observer

---
+++ vehicle-rental-project/src/main/java/edu/najah/software/observer/RentalObserver.java
@@ -0,0 +1,15 @@
+package edu.najah.software.observer;
+
+import edu.najah.software.model.Customer;
+import edu.najah.software.model.RentalRecord;
+import edu.najah.software.model.Vehicle;
+
+
+public interface RentalObserver {
+
+    void onRentalCreated(RentalRecord record, Customer customer, Vehicle vehicle);
+
+    void onRentalReturned(RentalRecord record, Customer customer, Vehicle vehicle);
+
+    void onExpiryReminderSent(RentalRecord record, Customer customer, String message);
+}
